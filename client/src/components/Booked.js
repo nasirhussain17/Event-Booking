3932a75bb09a4bfaa17fba1512897dcd2c586cd8
Md Nasir Husaain <nasirhussain17@gmail.com>
2020-04-21 17:27:07 +0530

    Booked Component

--- client/src/components/Booked.js
+++ client/src/components/Booked.js
@@ -7,11 +7,13 @@ const Book = (props) => (
   <Fragment>
     <div className="row  mt-5">
       <div className="col">
+        <h3>Event Name:{props.event.title}</h3>
+        <h4>Booked by:{props.user.name}</h4>
         BookingId: {props.book._id}
-        <h2>
+        <h4>
           Date Booked:<Moment>{props.book.createdAt}</Moment>
           <br />
-        </h2>
+        </h4>
       </div>
     </div>
   </Fragment>
@@ -21,8 +23,9 @@ class Booked extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { booked: [], user: "" };
+    this.state = { booked: [], user: "", event: "" };
     this.user = this.user.bind(this);
+    this.event = this.event.bind(this);
   }
 
   componentDidMount() {
@@ -40,14 +43,22 @@ class Booked extends Component {
       this.setState({ user: res.data });
     });
   }
+  event(id) {
+    axios.get(`http://localhost:5000/event/${id}`).then((res) => {
+      this.setState({ event: res.data });
+    });
+  }
 
   bookList() {
     return this.state.booked.map((currentbook) => {
+      this.user(currentbook.user);
+      this.event(currentbook.event);
       return (
         <Book
           book={currentbook}
           key={currentbook._id}
-          user={this.user(currentbook.user)}
+          user={this.state.user}
+          event={this.state.event}
         />
       );
     });
